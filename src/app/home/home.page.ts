2cd2ebff2384fd9becc873c4361d47177316956d
A. A. Sumitro <hello@aasumitro.id>
2022-04-29 16:37:01 +0800

    ionic view

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import zoomAndroid from '../../plugins/zoom-android.plugin';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-home',
@@ -7,17 +8,28 @@ import zoomAndroid from '../../plugins/zoom-android.plugin';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  enableCamera = true;
+  enableMicrophone = true;
 
   constructor() {}
+
 
   // ! ADD TOKEN
   // ? GET TOKEN FROM ZOOM WEB SAMPLE
   async performEnterConsultationRoom() {
+    const now = moment();
+    const appointmentSessionStartAt = now.unix();
+    const appointmentSessionEndAt = now.add(10, 'minutes').unix();
+
     zoomAndroid.tryJoinMeeting({
       // eslint-disable-next-line max-len
-      appointmentToken: '',
+      appointmentToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfa2V5IjoiS2p5aVgwTWEyM0JOb0pCMjZZUXB4Z1ZVSG5qcjNTRmlJc09rIiwiaWF0IjoxNjUxMjE4Nzk0LCJleHAiOjE2NTEyMjU5OTQsInRwYyI6InRlc05hbWUxMjMiLCJwd2QiOiIiLCJ1c2VyX2lkZW50aXR5IjoiIiwic2Vzc2lvbl9rZXkiOiIiLCJyb2xlX3R5cGUiOjF9.JDIvEqjAGyGCctl6BkIjBKVzoJFAHji5SLBcLqZIvWQ',
       appointmentSessionName:'tesName123',
-      customerFullName: 'Lorem Soparto'
+      customerFullName: 'Lorem Soparto',
+      enableCamera: this.enableCamera,
+      enableMicrophone: this.enableMicrophone,
+      appointmentSessionStartAt,
+      appointmentSessionEndAt
     });
   }
 }
